6b75c02cc21bb4ebf8fded57eb0d54b3d6eb4970
SENKWATE <m_alsaiegh@hotmail.com>
2022-03-27 19:24:44 +0300

    Second push

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-import sys, string, os
-import numpy as np 
+import os
 
-
-os.system("./conversion/pas2asc.exe")
+#  Classes
+from objects import Floor
 
 # utility functions
-from functions import julian_2_standard_date
+from functions import find_julian_day
 
 
-root_files = os.listdir('./input_files')
+root_file = "2023199"
+# print(find_julian_day(int(root_file[0:4]),int(root_file[4:7])))
 
+# X1 = []
+# X2 = []
+# X3 = []
+# Y1 = []
+# Y2 = []
+# Y3 = []
+# floor_names = ["F76","F65","F54","F42","F29","F16","F6","FB2"]
 
-os.system("pyinstaller ./conversion/pas2asc.exe")
-# for root_file in root_files:
-#     root_date = julian_2_standard_date(root_file)
-    # for data_logger in os.listdir('./input_files/'+root_file):
-        
+# for floor in floor_names:
+#     for coordinate in ["X1","Y1"]:
+#         globals()[coordinate] = []
+#         coordinate.append(Floor(floor,[]))
+
+# for name in floor_names:
+#     X1.append(Floor(name,[]))
+#     Y1.append(Floor(name,[]))
+#     X2.append(Floor(name,[]))
+#     Y2.append(Floor(name,[]))
+#     X3.append(Floor(name,[]))
+#     Y3.append(Floor(name,[]))
+
+# print(X1)
+# for i in X1:
+#     print("%s:"%i.name,i.value)
---
+++ objects.py
@@ -0,0 +1,5 @@
+class Floor:
+  def __init__(self, name,value):
+    self.name = name
+    self.value = value
+
